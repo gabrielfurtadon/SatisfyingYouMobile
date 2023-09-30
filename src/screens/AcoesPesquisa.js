bd4b4a9a2b3ab6917e25a094c6cdf84983217b2d
ViniciusGR797 <loritoamarelo@outlook.com>
2023-09-30 20:50:08 -0300

    Correção

--- src/screens/AcoesPesquisa.js
+++ src/screens/AcoesPesquisa.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { DefaultSquareDiv } from '../components/DefaultSquareDiv';
-import { StyleSheet, View } from 'react-native';
-
 
 export default function AcoesPesquisa(props) {
 
